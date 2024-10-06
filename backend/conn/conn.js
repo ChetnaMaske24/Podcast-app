8dc1286520763de998f31ab5c8bbc4f4d4e6475c
Chetna <cmaske580@gmail.com>
2024-10-06 18:01:58 +0530

    Start Implementing Backend

---
+++ backend/conn/conn.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const conn = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URL}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
