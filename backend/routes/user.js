f190742962b9c93ce98b14cce2f41a61bc2b0bba
Chetna <cmaske580@gmail.com>
2024-10-08 13:48:18 +0530

    sign-in functionality done

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 //signup - route
 router.post("/sign-up", async (req, res) => {
@@ -41,7 +42,50 @@ router.post("/sign-up", async (req, res) => {
     await newUser.save();
     return res.status(200).json({ message: "Account created" });
   } catch (error) {
-    res.status(400).json({ error });
+    res.status(500).json({ error });
+  }
+});
+
+//signin - route
+router.post("/sign-in", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    //check user exists
+    const existingUser = await User.findOne({ email: email });
+    if (!existingUser) {
+      return res.status(400).json({ message: "Invalid credential" });
+    }
+
+    //check password is matched or not
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credential" });
+    }
+
+    //Generate JWT Token
+    const token = jwt.sign(
+      { id: existingUser._id, email: existingUser.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "90d" }
+    );
+    res.cookie("podcasterUserToken", token, {
+      httpOnly: true,
+      maxAge: 90 * 24 * 60 * 60 * 100,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "None",
+    });
+
+    return res.status(200).json({
+      id: existingUser._id,
+      username: existingUser.username,
+      email: email,
+      message: "Sign-in Successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ error });
   }
 });
 
