cd8f64691832e160402011de8989512043900f1f
Chetna <cmaske580@gmail.com>
2024-10-18 20:51:47 +0530

    done Podcast API

--- backend/routes/podcast.js
+++ backend/routes/podcast.js
@@ -3,6 +3,7 @@ const authMiddleware = require("../middleware/authMiddleware");
 const Category = require("../models/category");
 const User = require("../models/user");
 const Podcast = require("../models/podcast");
+const Category = require("../models/category");
 const router = require("express").Router();
 
 // add-podcast
@@ -40,6 +41,69 @@ router.post("/add-podcast", authMiddleware, upload, async (req, res) => {
     res.status(201).json({ message: "Podcast added successfully" });
   } catch (error) {
     return res.status(500).json({ message: "Failed to add podcast" });
+  }
+});
+
+//get all podcast
+router.get("/get-podcasts", async (req, res) => {
+  try {
+    const podcasts = await Podcast.find()
+      .populate("category")
+      .sort({ createdAt: -1 });
+    return res.status(200).json({ data: podcasts });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+//get-user-podcast
+router.get("/get-user-podcasts", authMiddleware, async (req, res) => {
+  try {
+    const { user } = req;
+    const userid = user._id;
+    const data = await User.findById(userid)
+      .populate({
+        path: "podcasts",
+        populate: { path: "category" },
+      })
+      .select("-password");
+    if (data && data.podcasts) {
+      data.podcasts.sort(
+        (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+      );
+    }
+    return res.status(200).json({ data: data.podcasts });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// get podcast by id
+router.get("/get podcast/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const podcasts = await Podcast.findById(id).populate("category");
+    return res.status(200).json({ data: podcasts });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+//get podcast by categories
+router.get("/category/:cat", async (req, res) => {
+  try {
+    const { cat } = req.params;
+    const categories = await Category.find({ categoryName: cat }).populate({
+      path: "podcasts",
+      populate: { path: "category" },
+    });
+    let podcasts = [];
+    categories.forEach((category) => {
+      podcasts = [...podcasts, ...category.podcasts];
+    });
+    return res.status(200).json({ data: podcasts });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
   }
 });
 module.exports = router;
