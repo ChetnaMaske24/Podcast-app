cb62152e496ec945614f0938c74311198da01812
Chetna <cmaske580@gmail.com>
2024-10-22 21:57:35 +0530

    Start Implemeting Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 
 const Navbar = () => {
+  const navLinks = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "Categories",
+      path: "/categories",
+    },
+    {
+      name: "All Podcasts",
+      path: "/all-podcasts",
+    },
+  ]
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <div>
+        <div className='logo brand-name'>
+          <h1>Podcast</h1>
+        </div>
+        <div className=''></div>
+        <div className=''></div>
+      </div>
+    </nav>
   )
 }
 
