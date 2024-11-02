7176b7564bab48c2dc2affbf281c732af04aefdb
Chetna <cmaske580@gmail.com>
2024-11-02 21:49:52 +0530

    Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -16,16 +16,26 @@ const Navbar = () => {
     },
   ]
   return (
-    <nav>
-      <div>
-        <div className='logo brand-name'>
-          <h1>Podcast</h1>
+    <nav className='px-4 md:px-8 lg:px-12 py-2'>
+      <div className='flex items-center justify-between'>
+        <div className='logo brand-name w-2/6'>
+          <Link to="/" className='text-2xl font-bold'>Podcast</Link>
         </div>
-        <div className=''></div>
-        <div className=''></div>
+        <div className='w-2/6 flex justify-center'>
+        {navLinks.map((items, i)=>(
+          <Link
+          key={i}
+          to={items.path}
+          className="ms-4 hover:font-semibold transition-all duration-300"
+          >
+            {items.name}
+          </Link>
+        ))}
+        </div>
+        <div className='w-2/6'></div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar
